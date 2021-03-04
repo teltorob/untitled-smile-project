983c8c7cca5bb8f7af2c4d268e2789c148c3114d
Ayush Tiwari <teltorob@gmail.com>
2021-03-05 00:45:49 +0530

    color schemes added

--- frontend/complimentor.js
+++ frontend/complimentor.js
@@ -1,5 +1,18 @@
 const receiver = document.getElementById("receiver")
+const contentContainer = document.getElementById("content-container")
 
+var colourScheme= ["#fbd35b","#fb9d5b","#5bfb76","#5bfb76"]
+function colourSelector()
+{
+    let random = Math.floor(Math.random() * 5);
+    console.log(random)
+    let selectedColour = colourScheme[random]
+    console.log(selectedColour)
+    receiver.style.backgroundColor =selectedColour
+    contentContainer.style.color = selectedColour
+}
+
+colourSelector()
 
 var blink = setInterval(function() {
     if(receiver.placeholder== "Be a reason someone smiles today...")
