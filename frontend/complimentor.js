4c27ca8f3fc08c9b8e4afc1ac100aff540ba62d2
Ayush Tiwari <teltorob@gmail.com>
2021-03-05 00:14:15 +0530

    rough idea

---
+++ frontend/complimentor.js
@@ -0,0 +1,12 @@
+const receiver = document.getElementById("receiver")
+
+
+var blink = setInterval(function() {
+    if(receiver.placeholder== "Be a reason someone smiles today...")
+    receiver.placeholder= " "
+
+    else
+    receiver.placeholder="Be a reason someone smiles today..."
+
+},1300
+)
