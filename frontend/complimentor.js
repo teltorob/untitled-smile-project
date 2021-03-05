c611ab9f4b27c90aa370cf42f0ccb653ccc6064a
Shivangi Tiwari <shivangitiwari@Shivangis-MacBook-Pro.local>
2021-03-05 13:25:00 +0530

    js refined

--- frontend/complimentor.js
+++ frontend/complimentor.js
@@ -1,36 +1,35 @@
-const receiver = document.getElementById("receiver")
-var isReceiverFocused = (document.activeElement === receiver);
-console.log(isReceiverFocused)
-const contentContainer = document.getElementById("content-container")
-
-var colourScheme= ["#fbd35b","#fb9d5b","#5bfb76","#5bfb76","#fb5b79","#5b9bfb","#e462b6"]
-function colourSelector()
-{
-    let random = Math.floor(Math.random() * 7);
-    console.log(random)
-    let selectedColour = colourScheme[random]
-    console.log(selectedColour)
-    receiver.style.backgroundColor =selectedColour
-    contentContainer.style.color = selectedColour
+const receiver = document.getElementById("receiver");
+const contentContainer = document.getElementById("content-container");
+const placeholderMessage = "Be a reason someone smiles today...";
+var colourScheme = [
+  "#fbd35b",
+  "#fb9d5b",
+  "#5bfb76",
+  "#5bfb76",
+  "#fb5b79",
+  "#5b9bfb",
+  "#e462b6",
+];
+function colourSelector() {
+  let random = Math.floor(Math.random() * 7);
+  console.log(random);
+  let selectedColour = colourScheme[random];
+  console.log(selectedColour);
+  receiver.style.backgroundColor = selectedColour;
+  contentContainer.style.color = selectedColour;
 }
 
-colourSelector()
+colourSelector();
 
-var blink = setInterval(function() {
-    var isReceiverFocused = (document.activeElement === receiver);
+blinker();
+function blinker() {
+  blink = setInterval(function () {
+    if (receiver.placeholder == placeholderMessage) receiver.placeholder = " ";
+    else receiver.placeholder = placeholderMessage;
+  }, 1300);
+}
 
-    if(receiver.placeholder== "Be a reason someone smiles today...")
-    receiver.placeholder= " "
-
-    else
-    receiver.placeholder="Be a reason someone smiles today..."
-
-    if(isReceiverFocused)
-    {
-        receiver.placeholder= " "
-        clearInterval(blink)
-    }
-    
-
-},1300
-)
+function stopBlinker() {
+  receiver.placeholder = " ";
+  clearInterval(blink);
+}
