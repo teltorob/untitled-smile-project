aaecb51a5b601b6a84cdb33212c4d2e41053a650
Ayush Tiwari <teltorob@gmail.com>
2021-03-05 10:45:47 +0530

    new color scheme added

--- frontend/complimentor.js
+++ frontend/complimentor.js
@@ -1,10 +1,12 @@
 const receiver = document.getElementById("receiver")
+var isReceiverFocused = (document.activeElement === receiver);
+console.log(isReceiverFocused)
 const contentContainer = document.getElementById("content-container")
 
-var colourScheme= ["#fbd35b","#fb9d5b","#5bfb76","#5bfb76"]
+var colourScheme= ["#fbd35b","#fb9d5b","#5bfb76","#5bfb76","#fb5b79","#5b9bfb","#e462b6"]
 function colourSelector()
 {
-    let random = Math.floor(Math.random() * 5);
+    let random = Math.floor(Math.random() * 7);
     console.log(random)
     let selectedColour = colourScheme[random]
     console.log(selectedColour)
@@ -15,11 +17,20 @@ function colourSelector()
 colourSelector()
 
 var blink = setInterval(function() {
+    var isReceiverFocused = (document.activeElement === receiver);
+
     if(receiver.placeholder== "Be a reason someone smiles today...")
     receiver.placeholder= " "
 
     else
     receiver.placeholder="Be a reason someone smiles today..."
+
+    if(isReceiverFocused)
+    {
+        receiver.placeholder= " "
+        clearInterval(blink)
+    }
+    
 
 },1300
 )
