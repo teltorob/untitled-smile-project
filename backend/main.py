aa1d7996e3325e904027eadc35cd06ce25ec56bd
itsnotashes <itsnotamit@gmail.com>
2021-03-06 00:13:10 +0530

    did the backend stuff

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI, Form
+from fastapi.middleware.cors import CORSMiddleware
 
-
+import os
 import json
 from random import randint
 
 app = FastAPI()
 
-databasePath = r'I:\untitled-smile-project\backend\compliments.json'
+origins = ["*", "http://localhost:8080/"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#
+BASE_PATH = os.getcwd()
+databasePath = os.path.join(BASE_PATH, 'compliments.json')
 
 
 @app.get("/")
