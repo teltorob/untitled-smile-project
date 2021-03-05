f94d2c547de4c9b0f21c7af0f16ed8eec7cc2229
Shivangi Tiwari <shivangitiwari@Shivangis-MacBook-Pro.local>
2021-03-05 15:39:02 +0530

    responsive beta

--- frontend/complimentor.js
+++ frontend/complimentor.js
@@ -5,15 +5,14 @@ const placeholderMessage = "Be a reason someone smiles today...";
 var colourScheme = [
   "#fbd35b",
   "#fb9d5b",
-  "#5bfb76",
-  "#5bfb76",
-  "#fb5b79",
+  "#62e484",
   "#5b9bfb",
   "#e462b6",
+  "black",
 ];
 
 function colourSelector() {
-  let random = Math.floor(Math.random() * 7);
+  let random = Math.floor(Math.random() * 6);
   console.log(random);
   let selectedColour = colourScheme[random];
   console.log(selectedColour);
